72b471fed8bdc48c9682d6f2f1ff659e85b4a97b
christof <christof@kuhlmeyer.net>
2015-02-08 20:59:46 +0100

    Fixed energy display..

--- src/main/java/net/kuhlmeyer/hmlib/device/HMESPMSW1PL.java
+++ src/main/java/net/kuhlmeyer/hmlib/device/HMESPMSW1PL.java
@@ -11,12 +11,12 @@ import org.apache.log4j.Logger;
  * with Channels:
  *
  CH1 Toggle on/ff
- CH2 Strom-/Spannungs-/Leistungs- und Frequenzmesser -> sendet aktuelle Verbrauchs/Zustandswerte bei über/unterschreiten von definierten Schwellwerten
+ CH2 Strom-/Spannungs-/Leistungs- und Frequenzmesser - sendet aktuelle Verbrauchs/Zustandswerte bei über/unterschreiten von definierten Schwellwerten
  -- nur für PEER-TO-PEER
- CH3 Leistungs-Sensor -> sendet Event wenn definierte Leistung über/unterschritten -> UC: Waschmaschine oder Trockner fertig -> Push-Nachricht
- CH4 Strom-Sensor -> Analog CH3
- CH5 Spannungs-Sensor -> Analog CH3
- CH6 Frequenz-Sensor -> Analog CH3
+ CH3 Leistungs-Sensor - sendet Event wenn definierte Leistung über/unterschritten - UC: Waschmaschine oder Trockner fertig - Push-Nachricht
+ CH4 Strom-Sensor - Analog CH3
+ CH5 Spannungs-Sensor - Analog CH3
+ CH6 Frequenz-Sensor - Analog CH3
 
  */
 public class HMESPMSW1PL extends HMSwitch {
@@ -45,19 +45,19 @@ public class HMESPMSW1PL extends HMSwitch {
 
         // Seems to be Channel 1 Handling - Set ON/OFF
         if (24 == payload.length()) {
-            //12A4102C8A2A29A4E80601 00 00  -> 00 set OFF
-            //14A4102C8A2A29A4E80601 C8 00  -> C8 set ON
+            //12A4102C8A2A29A4E80601 00 00  - 00 set OFF
+            //14A4102C8A2A29A4E80601 C8 00  - C8 set ON
             return super.eventReceived(data);
         }
 
         else if (40 == payload.length()) {
 /*
-            $eCnt = ($eCnt&0x7fffff)/10;          #0.0  ..838860.7  Wh    -> 3 BYTE
-            $P = $P   /100;                       #0.0  ..167772.15 W     -> 3 BYTE
-            $I = $I   /1;                         #0.0  ..65535.0   mA    -> 2 BYTE
-            $U = $U   /10;                        #0.0  ..6553.5    mV    -> 2 BYTE
-            $F -= 256 if ($F > 127);                                      -> 1 BYTE
-            $F = $F/100+50;                       # 48.72..51.27     Hz   -> 1 BYTE
+            $eCnt = ($eCnt&0x7fffff)/10;          #0.0  ..838860.7  Wh    - 3 BYTE
+            $P = $P   /100;                       #0.0  ..167772.15 W     - 3 BYTE
+            $I = $I   /1;                         #0.0  ..65535.0   mA    - 2 BYTE
+            $U = $U   /10;                        #0.0  ..6553.5    mV    - 2 BYTE
+            $F -= 256 if ($F > 127);                                      - 1 BYTE
+            $F = $F/100+50;                       # 48.72..51.27     Hz   - 1 BYTE
 
               0  2  4  6  8  10 12 14 16    18 20 22 24 26 28 30 32 34 36 38
               3D 84 5E 2C 8A 2A 00 00 00    80 00 56 00 00 11 00 07 08 BF FE
